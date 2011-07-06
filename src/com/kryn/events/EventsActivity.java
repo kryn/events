c31e99c4491a755175c6461e11346c2e6b959b4c
mike <kryn@gmx.net>
2011-07-06 19:14:39 +0200

    initial commit

---
+++ src/com/kryn/events/EventsActivity.java
@@ -0,0 +1,13 @@
+package com.kryn.events;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EventsActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
